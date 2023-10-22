3096c4ca8fc26d5e6fc8fd0c0809fe5f92c827e6
Caio Novais <caionovais@usp.br>
2023-10-22 11:02:28 -0300

    add transparency to polybar and blue color. Add CaskaydiaCode nerd font

--- nvim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
@@ -2,6 +2,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup()
 
-vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>')
+-- vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<A-1>', ':NvimTreeFindFileToggle<CR>')
 -- Enable NERDTreeTabs
 vim.g.NERDTreeTabsCloseOnOpen = 0
